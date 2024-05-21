e37cc834e7c30dd9f57cded0b34ce3472ef76eff
kikadf <kikadf.01@gmail.com>
2024-05-21 14:38:19 +0200

    Autologin and autostart wm, patches moved from zsh folder, add help function, add arg to update only zsh stuff

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ _D_update="0"
 _D_os=$(uname -s)
 _D_zsh_confdir="$HOME/.config/zsh.d"
 _D_basedir=$(pwd)
-_D_patchdir="${_D_basedir}/zsh/patches"
+_D_patchdir="${_D_basedir}/patches"
 _D_OpenBSD_deps="linuxlibertine-fonts-ttf feh zsh breeze-icons rofi fluxbox \
                  zsh-syntax-highlighting noto-nerd-fonts cmake gmake wget binutils"
 _D_FreeBSD_deps="zsh zsh-autosuggestions zsh-syntax-highlighting zsh-completions \
@@ -15,7 +15,6 @@ _D_FreeBSD_deps="zsh zsh-autosuggestions zsh-syntax-highlighting zsh-completions
 _D_NetBSD_deps="linux-libertine-ttf feh zsh zsh-autosuggestions zsh-syntax-highlighting \
                 zsh-completions breeze-icons rofi fluxbox nerd-fonts-Meslo cmake gmake \
                 wget binutils perl"
-mkdir external
 
 # Read arguments
 for _arg in "$@"; do
@@ -25,6 +24,12 @@ for _arg in "$@"; do
             ;;
         -u)
             _D_update="1"
+            ;;
+        -U)
+            _D_update="2"
+            ;;
+        -h)
+            _D_debug="2"
             ;;
     esac
 done
@@ -67,7 +72,9 @@ patching() {
 
 cleaning() {
     msg "Clean build directory..."
-    rm -rf build
+    cd "$_D_basedir" || return 1
+    rm -rf external || return 1
+    return 0
 }
 
 # Install dependencies from binary repository
@@ -217,28 +224,76 @@ add_groups() {
     return 0
 }
 
-# Work
-if [ $_D_update -eq 0 ]; then
-    msg "Install dependencies:"
-    install_deps || die "Installing dependencies"
-fi
-msg "Start build and install Fluxbox-mod:"
-build_mzc || die "Building mozilla-zsh-config"
-build_zhss || die "Building zsh-history-substring-search"
-build_p10k || die "Building powerlevel10k theme"
-if [ "$_D_os" = "OpenBSD" ]; then
-    build_zc || die "Building zsh-completions"
-    build_zas || die "Building zsh-autosuggestions"
-fi
-build_fluxbox ||  die "Install fluxbox configs"
-build_rofi ||  die "Install rofi configs"
-add_groups ||  die "Set user groups"
+# Set default shell
+set_zsh() {
+    msg "Set zsh to default shell"
+    if [ "$_D_os" = "NetBSD" ]; then
+        chpass -s zsh || return 1
+    else
+        msg "... not changed."
+    fi
+    return 0
+}
 
-if [ $_D_debug -eq 0 ]; then
-    cleaning
-    msg "Ready."
+# Set autologin and autostart X
+set_autostart() {
+    install -m644 config/xorg/xinitrc.in "${HOME}/.xinitrc" || return 1
+    install -m644 config/zsh/zprofile.in "${HOME}/.zprofile" || return 1
+    cd / || return 1
+    # gettytab, ttys
+    patching system || return 1
+    return 0
+}
+
+# Help
+help() {
+    echo "Usage: build.sh [-u|-U|-h|-d]"
+    echo ""
+    echo "  -d      Don't clean external build directory after build"
+    echo "  -u      Don't install dependencies"
+    echo "  -U      Don't install config files exclude zsh"
+    echo "  -h      Show this help"
+    echo ""
+    echo "  Without arg make full build, install dependencies, install"
+    echo "  config files, build and install zsh stuff, clean build dir."
+}
+
+# Work
+main() {
+    mkdir external
+    if [ $_D_update -lt 2 ]; then
+        msg "Start build and install Fluxbox-mod:"
+        if [ $_D_update -eq 0 ]; then
+            msg "Install dependencies:"
+            install_deps || die "Installing dependencies"
+        fi
+        build_fluxbox ||  die "Install fluxbox configs"
+        build_rofi ||  die "Install rofi configs"
+        add_groups ||  die "Set user groups"
+        set_zsh || die "Set zsh to default shell"
+        set_autostart || die "Set autostart/login"
+    else
+        msg "Update Fluxbox-mod:"
+    fi
+    build_mzc || die "Building mozilla-zsh-config"
+    build_zhss || die "Building zsh-history-substring-search"
+    build_p10k || die "Building powerlevel10k theme"
+    if [ "$_D_os" = "OpenBSD" ]; then
+        build_zc || die "Building zsh-completions"
+        build_zas || die "Building zsh-autosuggestions"
+    fi
+    if [ $_D_debug -eq 0 ]; then
+        cleaning || die "Cleaning"
+        msg "Ready."
+    else
+        msg "Finished without cleaning."
+    fi
+}
+
+if [ $_D_debug -eq 2 ]; then
+    help
 else
-    msg "Finished without cleaning."
+    main
 fi
 
 exit 0
