3195b24c52c38ea868c149bbf565c1b02bca4f60
kikadf <kikadf.01@gmail.com>
2025-01-20 16:29:11 +0100

    Add keepassxc to autostarted applications

--- build.sh
+++ build.sh
@@ -14,7 +14,8 @@ _D_FreeBSD_deps="zsh zsh-autosuggestions zsh-syntax-highlighting zsh-completions
                  kf5-breeze-icons rofi fluxbox"
 _D_NetBSD_deps="linux-libertine-ttf feh zsh zsh-autosuggestions zsh-syntax-highlighting \
                 zsh-completions breeze-icons rofi fluxbox nerd-fonts-Meslo cmake gmake \
-                wget binutils perl wmctrl consolekit qt5ct picom"
+                wget binutils perl wmctrl consolekit qt5ct picom keepassxc gnome-keyring \
+                libsecret"
 
 # Read arguments
 for _arg in "$@"; do
